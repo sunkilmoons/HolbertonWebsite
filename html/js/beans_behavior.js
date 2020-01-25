a808c17b3474b397df3b8627c79d9b06f0cf8adb
sunkilmoons <bcassidy50@gmail.com>
2020-01-24 22:10:15 -0600

    Add more to brew page

---
+++ html/js/beans_behavior.js
@@ -0,0 +1,89 @@
+
+
+var triggered = false;
+const cherries_img = document.getElementById("coffee_cherry_img");
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    initBurgerClick();
+
+    window.onscroll = () => {
+
+        if (document.body.scrollTop < 300 && !triggered) {
+            console.log("less than 50");
+            triggered = true;
+        }
+        else if (document.body.scrollTop > 300 && !triggered) {
+            triggered = true;
+        }
+    };
+
+    const nav = document.querySelector(".nav_links");
+
+    window.addEventListener("resize", function () {
+        if (document.documentElement.clientWidth > 800) {
+            nav.style.display = "flex";
+            nav.style.transform = `translateX(0%)`;
+            isOpen = false;
+        } else {
+            nav.style.display = "none";
+            nav.style.transform = `translateX(100%)`;
+            isOpen = false;
+        }
+    }, true);
+});
+
+const initBurgerClick = () => {
+    const burger = document.getElementById("hamburgerBtn");
+    const nav = document.querySelector(".nav_links");
+    var isOpen = false;
+    var interval,
+        percentClosed = 100,
+        clock = 0;
+
+    burger.addEventListener("click", () => {
+        console.log("hit hamburger button");
+
+        clock = 0;
+
+        if (!isOpen) {
+            console.log("opening drawer");
+            nav.style.display = "flex";
+            isOpen = true;
+            clearInterval(interval);
+            interval = setInterval(openingDrawer, 1);
+        } else {
+            console.log("closing drawer");
+            isOpen = false;
+            clearInterval(interval);
+            interval = setInterval(closingDrawer, 1);
+        }
+
+        // nav.classList.toggle('nav_slide');
+    })
+
+    function closingDrawer() {
+        clock++;
+
+        percentClosed = clock * clock * 0.01;
+
+        if (percentClosed >= 100) {
+            clearInterval(interval);
+            nav.style.display = "none";
+        }
+
+        nav.style.transform = `translateX(${percentClosed}%)`
+    }
+
+    function openingDrawer() {
+        clock++;
+
+        percentClosed = 100 - (clock * clock * 0.01);
+
+        if (percentClosed <= 0) {
+            clearInterval(interval);
+        }
+
+        nav.style.transform = `translateX(${percentClosed}%)`
+    }
+}
